3499755ade0fb8db8ba2a4967be4e219e002a204
dr1179019@gmail.com <dr1179019@gmail.com>
2022-02-10 14:39:57 -0500

    septimo commit

--- app/Http/Controllers/HoteleController.php
+++ app/Http/Controllers/HoteleController.php
@@ -53,6 +53,25 @@ class HoteleController extends Controller
     /**
      * Display the specified resource.
      *
+     * 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
